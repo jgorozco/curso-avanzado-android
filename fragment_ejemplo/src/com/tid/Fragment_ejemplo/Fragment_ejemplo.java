d12ff654531bfceca5759d92e92d681a67164f89
jose Garcia Orozco <josegoupm@gmail.com>
2012-03-29 11:49:58 +0200

    init fragment ejemplo

---
+++ fragment_ejemplo/src/com/tid/Fragment_ejemplo/Fragment_ejemplo.java
@@ -0,0 +1,13 @@
+package com.tid.Fragment_ejemplo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Fragment_ejemplo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
